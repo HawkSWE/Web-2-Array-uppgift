3cb26d3cc203d870629723172bdd43a833fd89ed
HawkSWE <83948065+HawkSWE@users.noreply.github.com>
2022-09-02 09:36:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function sort(){
-
+    // Denna funktion används för att sortera en array av siffror.
+    // Den sorterar siffrorna så det blir minst till störst i arrayn.
     const sorterad_p = document.getElementById('sorterad')
     random.sort(function(a, b){return a-b});
     sorterad_p.innerText = random.join(', ')
 }
 
 function get_numbers(){
-
+    // Den här funktionen genererar 10 siffror mellan 1-100 och sedan sparar det i en array.
     while(random.length < 10){
         var r = Math.floor(Math.random() * 100) + 1;
+        // Den här if satsen kollar om en random siffra redan finns i arrayn så det inte blir fler av samma siffra.
         if (random.indexOf(r) === -1) random.push(r);
     }
 
